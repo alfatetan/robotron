cfa1d4bb2553c5439069f8a28e8d11d757980576
alfatetan <alfatetan@gmail.com>
2019-04-18 20:53:00 +0500

    Конфигурационный файл Robotron, который содержит все необходимые константы/переменные для работы

---
+++ robotron_conf.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# The basic constants for robotron
+
+PATH_SCHEME = "/var/lib/asterisk/sounds/robotron/"
+PATH_MONITOR = "/var/spool/asterisk/monitor/"
+PATH_CONFIGS = "/ets/asterisk/"
+PATH_AGI = "/var/lib/asterisk/agi-bin/"
