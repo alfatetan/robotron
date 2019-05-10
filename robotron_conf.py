661f142eeb33c22ba9adc9efabd37484df9ef474
alfatetan <alfatetan@gmail.com>
2019-05-10 16:24:06 +0500

    Общие переменные для системы Роботрон, изменяя которые можно конфигурировать систему.

--- robotron_conf.py
+++ robotron_conf.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 # The basic constants for robotron
 
-PATH_SCHEME = "/var/lib/asterisk/sounds/robotron/"
+PATH_SCHEME = "/var/lib/asterisk/sounds/robotron/test/"
+PATH_SCHEMES = "/var/lib/asterisk/sounds/robotron/"
 PATH_MONITOR = "/var/spool/asterisk/monitor/"
 PATH_CONFIGS = "/ets/asterisk/"
 PATH_AGI = "/var/lib/asterisk/agi-bin/"
+LOG_FILE = "/var/lib/asterisk/agi-bin/log_file.log"
